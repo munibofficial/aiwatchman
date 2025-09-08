9ebe0f7a4e814e504ab56f184057118a3a393644
Arqum Ali <arqumdoger@gmail.com>
2025-09-08 12:34:43 +0500

    after removeing code

--- app.py
+++ app.py
@@ -268,5 +268,5 @@ def hello():
 # # ================================
 # # STEP 4: Run
 # # ================================
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
